bf545ddb5e4718020ec3bc2103ecfd5e1fd74de8
dimas1976 <kogan.dmitriy@gmail.com>
2021-07-22 16:18:58 +0200

    Âdd form structure

---
+++ src/components/form-component/form.stories.js
@@ -0,0 +1,9 @@
+import "./form.css";
+import form from "./form.html";
+
+export default {
+  title: "Components/Forms",
+  parameters: { layout: "fullscreen" },
+};
+
+export const Form = () => form;
