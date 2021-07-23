25df46f15eb0c036958d6ac7fbf1a57bdc9f4fa0
dimas1976 <kogan.dmitriy@gmail.com>
2021-07-23 18:58:37 +0200

    Add html structure

---
+++ src/components/task-item-component/task-item-component.stories.js
@@ -0,0 +1,9 @@
+import "./task-item-component.css";
+import taskItem from "./task-item-component.html";
+
+export default {
+  title: "Components/Task Items",
+  parameters: { layout: "fullscreen" },
+};
+
+export const taskItems = () => taskItem;
