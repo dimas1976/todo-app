c529af2477382df84f909fedf7806db7f98d05b2
dimas1976 <kogan.dmitriy@gmail.com>
2021-07-26 12:04:00 +0200

    Remove space

--- src/components/pages/add-task-component/add-task.stories.js
+++ src/components/pages/add-task-component/add-task.stories.js
@@ -2,7 +2,7 @@ import "./add-task.css";
 import addTask from "./add-task.html";
 
 export default {
-  title: "Components/Add Task",
+  title: "Pages/Add Task",
   parameters: { layout: "fullscreen" },
 };
 
--- src/components/pages/tasks/tasks.stories.js
+++ src/components/pages/tasks/tasks.stories.js
@@ -2,7 +2,7 @@ import "./tasks.css";
 import tasks from "./tasks.html";
 
 export default {
-  title: "Components/Task Page",
+  title: "Pages/Task Page",
   parameters: { layout: "fullscreen" },
 };
 
