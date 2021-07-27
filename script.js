3b1aa9ec9cc7f4fa150cbf9b04a45b980fdd1f43
dimas1976 <kogan.dmitriy@gmail.com>
2021-07-27 15:26:45 +0200

    Create new task list item

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-let hello = "Hello People";
+const menuButton = document.querySelector(".menuButton");
 
-alert(hello);
+menuButton.onclick = sendAlert;
+function sendAlert() {
+  alert("Hellol World");
+}
+
+const taskList = document.querySelector(".tasksList");
+const newTask = document.createElement("li");
+newTask.innerText = "Example task";
+taskList.append(newTask);
+
+function createNewTaskListItem() {
+  const taskListItem = document.createElement("li");
+  taskListItem.className = "taskList_item";
+
+  const label = document.createElement("label");
+  label.className = "task-item";
+
+  const input = document.createElement("input");
+  input.className = "task-item__checkbox";
+
+  const span = document.createElement("span");
+  span.className = "task-item__text";
+
+  label.append(input);
+  label.append(span);
+
+  taskListItem.append(label);
+
+  return taskListItem;
+}
