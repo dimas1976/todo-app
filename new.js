dfe0d9cd3e187b0c5f8dcffca55e16e1afe8e48b
dimas1976 <kogan.dmitriy@gmail.com>
2021-07-28 15:47:29 +0200

    Add object

--- new.js
+++ new.js
@@ -1,14 +1,18 @@
 const form = document.querySelector(".form");
-const checkedInput = document.querySelector('input[type="text"]');
+const input = document.querySelector('input[type="text"]');
 const checkedRadio = document.querySelector('input[type="radio"]:checked');
 
 form.onsubmit = function (event) {
   event.preventDefault();
+  const newTaskObj = {
+    title: "",
+    date: "",
+    isDone: false,
+  };
+  newTaskObj.title = input.value;
+  newTaskObj.date = checkedRadio.value;
   console.log(
-    "Input value:: " +
-      checkedInput.value +
-      "\n" +
-      "Radio value:: " +
-      checkedRadio.value
+    `Input value ${newTaskObj.title} und 
+${newTaskObj.date}`
   );
 };
