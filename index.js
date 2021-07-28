112587b787639cb424e708d5364b9b6ad415f63c
dimas1976 <kogan.dmitriy@gmail.com>
2021-07-28 17:36:04 +0200

    Put data to list

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const menuButton = document.querySelector(".menuButton");
-
-menuButton.onclick = sendAlert;
-function sendAlert() {
-  alert("Hellol World");
-}
-
 const taskList = document.querySelector(".tasksList");
 
-const taskOne = createNewTaskListItem();
+const newTaskObj = {
+  title: "Listen to music",
+  date: "tomorrow",
+  isDone: true,
+};
+
+const taskOne = createNewTaskListItem(newTaskObj);
 
 taskList.append(taskOne);
 
-function createNewTaskListItem() {
+function createNewTaskListItem(task) {
   const taskListItem = document.createElement("li");
   taskListItem.className = "taskList_item";
 
@@ -21,11 +20,12 @@ function createNewTaskListItem() {
   const input = document.createElement("input");
   input.className = "task-item__checkbox";
   input.type = "checkbox";
+  input.checked = task.isDone;
   input.setAttribute("name", "tasks");
 
   const span = document.createElement("span");
   span.className = "task-item__text";
-  span.innerText = "Joggen abends 20km";
+  span.innerText = task.title;
 
   label.append(input, span);
 
